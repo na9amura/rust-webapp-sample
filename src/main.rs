6301df842a9aef0056ad62132079e4462cda42a2
na9amura <4410504+na9amura@users.noreply.github.com>
2022-08-11 01:20:11 +0000

    TLS/HTTPS

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,22 @@
-use std::sync::Mutex;
-use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
-
-struct AppState {
-    app_name: String,
-    counter: Mutex<i32>,
-}
+use actix_web::{App, HttpServer, get, HttpRequest, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
-}
-
-#[get("/count")]
-async fn count(data: web::Data<AppState>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-    format!("Request number: {counter}")
-}
-
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(index)
-       .service(count);
-}
-
-async fn app_index() -> impl Responder {
-    "Hello world!"
-}
-
-fn app_config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/index.html", web::get().to(app_index));
-}
-
-fn api_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/api")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("API") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Welcome!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let state = web::Data::new(AppState {
-        app_name: String::from("Actix Web"),
-        counter: Mutex::new(0),
-    });
+    // load TLS keys
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(state.clone())
-            .configure(config)
-            .service(
-                web::scope("/app").configure(app_config)
-            )
-            .configure(api_config)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .run()
+        .await
 }
