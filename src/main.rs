97a1086c8f588936c76a8f6c9c08c39f4e15d8f3
na9amura <4410504+na9amura@users.noreply.github.com>
2022-08-13 07:35:15 +0000

    [Request Handlers](https://actix.rs/docs/handlers/)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
-use actix_web::{App, HttpServer, get, HttpRequest, Responder};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix_web::{App, HttpServer, get, HttpRequest, Responder, body::BoxBody, HttpResponse, http::header::ContentType, Error, web, Either};
+use serde::Serialize;
+use futures::{future::ok, stream::once};
+
+#[derive(Serialize)]
+struct MyResponse {
+    name: &'static str,
+}
+
+impl Responder for MyResponse {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
-    "Welcome!"
+    MyResponse { name: "user" }
+}
+
+#[get("/stream")]
+async fn stream() -> HttpResponse {
+    let body = once(ok::<_, Error>(web::Bytes::from_static(b"test")));
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .streaming(body)
+}
+
+type RegisterResult = Either<HttpResponse, Result<&'static str, Error>>;
+
+#[get("/either")]
+async fn either(_req: HttpRequest) -> RegisterResult {
+    if true {
+        Either::Left(HttpResponse::BadRequest().body("Bad Request"))
+    } else {
+        Either::Right(Ok("Hello!"))
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // load TLS keys
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file("key.pem", SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file("cert.pem").unwrap();
-
-    HttpServer::new(|| App::new().service(index))
-        .bind_openssl("127.0.0.1:8080", builder)?
+    HttpServer::new(|| 
+            App::new()
+                .service(index)
+                .service(stream)
+                .service(either)
+        )
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
