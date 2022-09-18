d1b61ef5b1bf945be57b064eb7fa132bc0724804
na9amura <4410504+na9amura@users.noreply.github.com>
2022-09-18 15:15:33 +0900

    ignore secrets

--- migrations/20220918060241_add_messages.down.sql
+++ migrations/20220918060241_add_messages.down.sql
@@ -1,2 +1,2 @@
 -- Add down migration script here
-DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS messages ;
--- migrations/20220918060241_add_messages.up.sql
+++ migrations/20220918060241_add_messages.up.sql
@@ -1,5 +1,5 @@
 -- Add up migration script here
-CREATE TABLE IF NOT EXISTS posts (
+CREATE TABLE IF NOT EXISTS messages (
     id serial PRIMARY KEY,
     user_id INT REFERENCES users(id),
     created_at TIMESTAMP,
