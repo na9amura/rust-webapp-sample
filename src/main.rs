9762b4b78811a54868db146824c8cc9516586193
na9amura <4410504+na9amura@users.noreply.github.com>
2022-09-24 18:43:17 +0900

    prepare api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,22 @@
-use actix_web::{App, HttpServer, get, web, Result, HttpRequest};
+use actix_web::{App, HttpServer, post, web, Result};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct Info {
-    id: u32,
-    name: String,
+struct Post {
+    user_id: u32,
+    content: String,
 }
 
-#[get("/users/{user_id}/{friend}")]
-async fn friend(path: web::Path<(u32, String)>) -> Result<String> {
-    let (user_id, friend) = path.into_inner();
-    Ok(format!("Welcome {}, user_id: {}", friend, user_id))
-}
-
-#[get("/items/{id}/{name}")]
-async fn items(info: web::Path<Info>) -> Result<String> {
-    Ok(format!("Item {}, item_id: {}", info.name, info.id))
-}
-
-#[get("/unsafe/{user_id}/{friend}")]
-async fn unsafe_users(req: HttpRequest) -> Result<String> {
-    let name: String = req.match_info().get("friend").unwrap().parse().unwrap();
-    let user_id: i32 = req.match_info().query("user_id").parse().unwrap();
-    Ok(format!("Welcome {}, user_id: {}", name, user_id))
-}
-
-#[derive(Deserialize)]
-struct QueryParams {
-    username: String,
-}
-
-#[get("/users")]
-async fn users(info: web::Query<QueryParams>) -> String {
-    format!("Finding {} ...", info.username)
+#[post("/send_message")]
+async fn send_message(data: web::Json<Post>) -> Result<String> {
+    Ok(format!("{}: {}", data.user_id, data.content))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
             App::new()
-                .service(friend)
-                .service(items)
-                .service(unsafe_users)
-                .service(users)
+                .service(send_message)
         )
         .bind(("127.0.0.1", 8080))?
         .run()
