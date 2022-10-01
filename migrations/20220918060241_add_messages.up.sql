504a0fbb965ee3cb991af320c46f45d6fe5fc195
na9amura <4410504+na9amura@users.noreply.github.com>
2022-10-01 19:20:55 +0900

    add not null constraints

--- migrations/20220918060241_add_messages.up.sql
+++ migrations/20220918060241_add_messages.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS messages (
     id serial PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    created_at TIMESTAMP,
-    content TEXT
+    user_id INT REFERENCES users(id) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    content TEXT NOT NULL
 );
